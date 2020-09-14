9150c61542aecd84aa77441930a7af2be98d96e3
Travis Jefferson <traviscjefferson@gmail.com>
2020-09-14 14:23:17 -0400

    Rough idea / implementation and main test path
    Naming is all TBD

---
+++ src/__tests__/enhanced-screen.test.js
@@ -0,0 +1,35 @@
+const { screen } = require("@testing-library/dom");
+const { enhanceQueries } = require("../index");
+
+const renderIntoDocument = html => {
+  const container = document.createElement("div");
+  container.id = "render-container";
+  container.innerHTML = html;
+  document.body.appendChild(container);
+  return { unmount: () => document.body.removeChild(container) };
+};
+
+afterEach(() => {
+  document.body.innerHTML = "";
+});
+
+test("enhanced queries can reuse data for any of get/query/find query types", async () => {
+  const { unmount } = renderIntoDocument(
+    '<img src="/path/to/logo.png" alt="logo"/>'
+  );
+
+  const enhancedQueries = enhanceQueries(screen);
+  const logoData = { filter: "role", params: ["img", { name: "logo" }] };
+
+  await expect(enhancedQueries.find(logoData)).resolves.toBeTruthy();
+  expect(enhancedQueries.get(logoData)).toBeTruthy();
+  expect(enhancedQueries.query(logoData)).toBeTruthy();
+
+  unmount();
+
+  await expect(enhancedQueries.find(logoData)).rejects.toThrow(
+    /unable to find/i
+  );
+  expect(() => enhancedQueries.get(logoData)).toThrow(/unable to find/i);
+  expect(enhancedQueries.query(logoData)).toBeNull();
+});
