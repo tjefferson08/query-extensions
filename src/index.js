742bb6c6b04d1470efc27a8164254c4b303c4ded
Travis Jefferson <traviscjefferson@gmail.com>
2020-09-14 19:30:39 -0400

    Export install fn (and inline, it's small)

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ const { enhanceQueries } = require("./enhance-queries");
 
 domTestingLib.screen = enhanceQueries(screen);
 
-module.exports = { enhanceQueries };
+const install = () => {
+  domTestingLib.screen = enhanceQueries(screen);
+};
+
+module.exports = { enhanceQueries, install };
--- src/install.js
+++
@@ -1,8 +0,0 @@
-const domTestingLib = require("@testing-library/dom");
-const { enhanceQueries } = require("./enhance-queries");
-
-const install = () => {
-  domTestingLib.screen = enhanceQueries(screen);
-};
-
-module.exports = { install };
