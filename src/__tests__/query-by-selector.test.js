451f1be21e8e09a6813b8d3bc346f89784e28a05
Travis Jefferson <traviscjefferson@gmail.com>
2020-09-19 14:03:40 -0400

    Add new core query: *BySelector
    Which simple defers to `querySelector`. Sometimes you just need to hook into a
    class name / ID even though it stinks :/

---
+++ src/__tests__/query-by-selector.test.js
@@ -0,0 +1,46 @@
+const { renderIntoDocument } = require("../../test/utils");
+const {
+  queryBySelector,
+  queryAllBySelector,
+  getBySelector,
+  getAllBySelector,
+  findBySelector,
+  findAllBySelector
+} = require("../query-by-selector");
+
+test("should support all queries by standard css selectors", async () => {
+  const { unmount } = renderIntoDocument(
+    `<div class="wrapper">
+       <span class="child">Item 1</span>
+       <span class="child">Item 2</span>
+       <span class="child">Item 3</span>
+     </div>`
+  );
+
+  const container = document.body;
+  const ERR_MESSAGE = /unable to find/i;
+
+  expect(queryBySelector(container, ".wrapper")).toBeTruthy();
+  expect(queryAllBySelector(container, ".child")).toHaveLength(3);
+
+  expect(getBySelector(container, ".wrapper")).toBeTruthy();
+  expect(getAllBySelector(container, ".child")).toHaveLength(3);
+
+  await expect(findBySelector(container, ".wrapper")).resolves.toBeTruthy();
+  await expect(findAllBySelector(container, ".child")).resolves.toHaveLength(3);
+
+  unmount();
+
+  expect(queryBySelector(container, ".wrapper")).toBeNull();
+  expect(queryAllBySelector(container, ".child")).toHaveLength(0);
+
+  expect(() => getBySelector(container, ".wrapper")).toThrow(ERR_MESSAGE);
+  expect(() => getAllBySelector(container, ".child")).toThrow(ERR_MESSAGE);
+
+  await expect(findBySelector(container, ".wrapper")).rejects.toThrow(
+    ERR_MESSAGE
+  );
+  await expect(findAllBySelector(container, ".child")).rejects.toThrow(
+    ERR_MESSAGE
+  );
+});
---
+++ src/query-by-selector.js
@@ -0,0 +1,27 @@
+const { buildQueries } = require("@testing-library/dom");
+
+const getMultipleError = (c, selector) =>
+  `Found multiple elements with the selector text: ${selector}`;
+
+const getMissingError = (c, selector) =>
+  `Unable to find an element with the selector text: ${selector}`;
+
+const queryAllBySelector = (container, selector) =>
+  container.querySelectorAll(selector);
+
+const [
+  queryBySelector,
+  getAllBySelector,
+  getBySelector,
+  findAllBySelector,
+  findBySelector
+] = buildQueries(queryAllBySelector, getMultipleError, getMissingError);
+
+module.exports = {
+  queryBySelector,
+  queryAllBySelector,
+  getBySelector,
+  getAllBySelector,
+  findBySelector,
+  findAllBySelector
+};
